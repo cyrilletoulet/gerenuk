9b5ef1f57dd36af2f0be0fdd2f3e62b991e454e2
Cyrille TOULET <cyrille.toulet@univ-lille.fr>
2019-04-25 15:53:40 +0200

    Fix vcores/vram issue

--- gerenuk/monitoring/libvirtmon.py
+++ gerenuk/monitoring/libvirtmon.py
@@ -18,7 +18,7 @@
 #
 #
 # Cyrille TOULET <cyrille.toulet@univ-lille.fr>
-# Wed 24 Apr 07:45:24 CEST 2019
+# Thu 25 Apr 15:31:45 CEST 2019
 
 import multiprocessing
 import ConfigParser
@@ -124,7 +124,7 @@ class LibvirtMonitor():
         for domain_id in domain_ids:
             domain = self.connection.lookupByID(domain_id)
 
-            vcpu = domain.maxVcpus()
+            vcores = domain.maxVcpus()
             vram = domain.maxMemory() / 1024
 
             cpu_stats_1 = domain.getCPUStats(True)
@@ -136,14 +136,14 @@ class LibvirtMonitor():
             t1 = cpu_stats_1[0]["cpu_time"]
             t2 = cpu_stats_2[0]["cpu_time"]
 
-            cpu_usage = (t2 - t1) / vcpu / 10.**9 / self.config.getint("libvirtmon", "sampling_time")
+            cpu_usage = (t2 - t1) / vcores / 10.**9 / self.config.getint("libvirtmon", "sampling_time")
             real_cpu_usage = (t2 - t1) / self.hypervisor["cores"] / 10.**9 / self.config.getint("libvirtmon", "sampling_time")
             mem_stats_average = (mem_stats_1["actual"] + mem_stats_2["actual"]) / 2.
             mem_usage = (mem_stats_average / 1024 / self.hypervisor["estimated_memory"])
 
             stats = dict()
             stats["uuid"] = domain.UUIDString()
-            stats["vcpu"] = vcpu
+            stats["vcores"] = vcores
             stats["vram"] = vram
             stats["vcpu_usage"] = round(cpu_usage * 100., 2)
             stats["cpu_usage"] = round(real_cpu_usage * 100., 2)
@@ -183,11 +183,14 @@ class LibvirtMonitor():
 
                 if not uuid in self.monitoring:
                     self.monitoring[uuid] = {
+                        "info": {"vcores": 0, "vram": 0},
                         "hourly": {"vcpu": [], "cpu": [], "mem": []},
                         "daily": {"vcpu": [], "cpu": [], "mem": []},
                         "weekly": {"vcpu": [], "cpu": [], "mem": []}
                     }
 
+                self.monitoring[uuid]["info"]["vcores"] = int(vcores)
+                self.monitoring[uuid]["info"]["vram"] = int(vram)
                 self.monitoring[uuid]["hourly"]["vcpu"] += [float(n) for n in hourly_vcpu_usage.split(',')]
                 self.monitoring[uuid]["hourly"]["cpu"] += [float(n) for n in hourly_cpu_usage.split(',')]
                 self.monitoring[uuid]["hourly"]["mem"] += [float(n) for n in hourly_mem_usage.split(',')]
@@ -207,6 +210,7 @@ class LibvirtMonitor():
 
         if not uuid in self.monitoring:
             self.monitoring[uuid] = {
+                "info": {"vcores": stats["vcores"], "vram": stats["vram"]},
                 "hourly": {"vcpu": [], "cpu": [], "mem": []},
                 "daily": {"vcpu": [], "cpu": [], "mem": []},
                 "weekly": {"vcpu": [], "cpu": [], "mem": []}
@@ -277,7 +281,8 @@ class LibvirtMonitor():
 
                 sql = 'INSERT INTO instances_monitoring (' + ', '.join(fields) + ') VALUES ("%s", "%s", "%d", "%d"' + ', "%s"'*9 + ');'
 
-                values = (uuid, self.hypervisor["hostname"], self.hypervisor["cores"], self.hypervisor["estimated_memory"])
+                values = (uuid, self.hypervisor["hostname"])
+                values += (self.monitoring[uuid]["info"]["vcores"], self.monitoring[uuid]["info"]["vram"])
                 values += (str("%.1f" % self.monitoring[uuid]["hourly"]["vcpu"][0]), '', '')
                 values += (str("%.1f" % self.monitoring[uuid]["hourly"]["cpu"][0]), '', '')
                 values += (str("%.1f" % self.monitoring[uuid]["hourly"]["mem"][0]), '', '')
@@ -286,4 +291,3 @@ class LibvirtMonitor():
                 self.loaded_stats.append(uuid)
 
         self.database.commit()
-
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "gerenuk",
-    version = "1.0.0",
+    version = "1.0.1",
     description = "A cloud monitoring tools set",
     url = "https://github.com/cyrilletoulet/gerenuk",
 
